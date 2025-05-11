cb9b61bd3bc009a1cceabcbfe9c341072fde522a
Rist1 <153267932+Rist1@users.noreply.github.com>
2025-05-11 17:47:21 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,91 @@
+window.PorfolioConfig = {
+  name: 'LE BEAUDOUR Bastien',
+  jobTitle: 'Etudiant en Informatique',
+  jobDescription: "En recherche d'Alternance en développement logiciel.",
+  skills: [
+    {
+      name: 'Python',
+      description: "Bonne maîtrise, plusieurs projets réalisés dans ce langage. <br>4 ans d'expérience."
+    },
+    {
+      name: 'C',
+      description: "Bonne maîtrise, plusieurs projets réalisés dans ce langage. <br>1 an d'expérience."
+    },
+    {
+      name: 'Bash',
+      description: "Bonne maîtrise du scripting et de docker. <br>1 an d'expérience."
+    },
+    {
+      name: 'Java',
+      description: "Bonne maîtrise de la programmation objet. <br>1 an d'expérience."
+    },
+    {
+      name: 'SQL',
+      description: "Bonne maîtrise. <br>2 ans d'expériences."
+    }
+  ],
+  works: [
+    {
+      name: 'Développer des applications informatiques simples',
+      description: "Développement d'un bot Discord qui aide à la gestion de parties de jeu de rôle en ligne."
+    },
+    {
+      name: 'Appréhender et construire des algorithmes',
+      description: "Développement d'un algorithme automatisant des parties d'un snake modifié avec des obstacles et des téléporteurs en C."
+    },
+    {
+      name: 'Installer et configurer un poste de travail',
+      description: "Création de scripts bash permettant l'automatisation de la transformation de fichiers pour un site web en utilisant Docker."
+    },
+    {
+      name: "Concevoir et mettre en place une base de données à partir d'un cahier des charges client",
+      description: "Création d'une base de donnée permettant la gestion d'une saison de Football avec différentes ligues."
+    },
+    {
+      name: 'Identifier les besoins métiers des clients et des utilisateurs',
+      description: "Création d'un site web sur les Jeux Olympiques de Paris 2024 à partir de besoins clients."
+    },
+    {
+      name: 'Identifier ses aptitudes pour travailler dans une équipe',
+      description: "Création d'une journée teambuilding et d'une vidéo pour la promouvoir."
+    }
+  ],
+  etudes: [
+    {
+      name: 'BUT Informatique 2024-2027',
+      description: "BUT Informatique à l'IUT de Lannion."
+    },
+    {
+      name: 'Lycée Général 2021-2024',
+      description: "Bac spécialité mathématiques et NSI au Lycée Rabelais de Saint-Brieuc."
+    }
+  ],
+  hobbies: [
+    {
+      name: 'Programmation',
+      description: "Programmer des logiciels et des algorithmes est passionnant."
+    },
+    {
+      name: 'Géopolitique',
+      description: "Il y a bien trop d'événements importants actuellement pour ne pas s'y intéresser."
+    },
+    {
+      name: 'Jeux Vidéos',
+      description: "Palmarès:<br>A battu le Pantheon d'Hallownest en no-hit sur Hollow Knight. <br>Meilleur Support d'Europe sur Overwatch Switch en 2020. <br>Apex Predator sur Apex Legends en Saison 4. <br>A atteint 47 de Chaleur sur Hades avec les gants de Gilgamesh."
+    }
+  ],
+  contacts: {
+    email: 'bastien.lebeaudour@etudiant.univ-rennes.fr',
+    mobileNo: '06 80 00 53 38',
+    links: [
+      {
+        name: 'CV',
+        link: 'Bastien_LE_BEAUDOUR_CV.pdf'
+      },
+      {
+        name: 'Linkedin',
+        link: 'https://www.linkedin.com/in/bastien-le-beaudour-a92608355/'
+      },
+    ]
+  }
+};
